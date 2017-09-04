6259392e0c77811850548bcbdaa03a773735f4e4
YongJin Jung <30520764+greensori@users.noreply.github.com>
2017-09-04 17:40:50 +0900

    simplestepper controller

--- controller.py
+++ controller.py
@@ -4,17 +4,14 @@ import time
 
 ard = serial.Serial('COM3', 115200)
 
-def keyup(e):
-    print 'up', e.char
-
 def keydown(e):
-    print 'down', e.char
+    print 'keypress', e.char
     ard.write(e.char)
 
 root = Tk()
 frame = Frame(root, width=100, height=100)
 frame.bind("<KeyPress>", keydown)
-frame.bind("<KeyRelease>", keyup)
+#frame.bind("<KeyRelease>", keyup)
 frame.pack()
 frame.focus_set()
 root.mainloop()
