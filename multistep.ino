d6479925872801980c49ff09baabf3c810307ff2
YongJin Jung <30520764+greensori@users.noreply.github.com>
2017-09-07 20:58:19 +0900

    Update multistep.ino

--- multistep.ino
+++ multistep.ino
@@ -2,6 +2,8 @@
 //must setting new (int totalswitch)
 //char totalswitch = a ~ c (1 stepping) d, e, f(1-2), (2-3), (1-3) g(1-2-3)
 //stepset() -> setting pulse -> end
+//before stepping operateion ending, must send serial signals
+//step = pulse * 2
 
 #define ENA1    5
 #define DIR1    6
@@ -19,10 +21,14 @@ char ch;
 int count;
 //steps, rpm, max, acc
 int totalswitch;
-//setting 3stepping motors (steps, pulse, maxpulse, acc, onoff)
-int vstep1[4] = {110, 900, 470, 0, 0};
-int vstep2[4];
-int vstep3[4];
+//setting 3stepping motors (steps, pulse, maxpulse, acc)
+//int vstep[3];
+int vpulse[3];
+int vlimit[3];
+int vacc[3];
+int vdir[3];
+int signal;
+
 
 void setup() {
     Serial.begin(115200);
@@ -62,54 +68,19 @@ void loop() {
 
 //to give a total switch values
 void charconverter() {
-  if (ch == 'q') {
-    vstep[0] = 110;
-  } else if (ch == 'w') {
-    vstep[0] = 30;
-  } else if (ch == 'e') {
-    vstep[0] = 2;
-  } else if (ch == 'r') {
-    vstep[0] = 50;
-  } else if (ch == 't') {
-  } else if (ch == 'u') {
-    vstep[0] = 110;
-  } else if (ch == 'i') {
-    vstep[0] = 20;
-  } else if (ch == 'o') {
-    vstep[0] = 20;
-  } else if (ch == 'p') {
-    vstep[0] = 20;
-  } else if (ch == 'a') {
-    vstep[0] = 20;
-  } else if (ch == 's') {
-    vstep[0] = 20;
+  switch (ch) {
+      case 'q':
+          step[0] = 50;
+      case 'w':
+          step[0] = 100;
   }
+           signal = (step[0] / 2)
 }
 
+//
 void stepset() {
-  if (ch == '0') {
-    digitalWrite(DIR, LOW);
-    vstep[3] = 0;
-  } else if(ch == '1') {
-    vstep[3] = 1;
-  } else if ( ch == '2') {
-    vstep[3] = 2;
-  } else if (ch == '3') {
-    vstep[3] = 3;
-  } else if (ch == '4') {
-    vstep[3] = 4;
-  } else if (ch == '5') {
-    digitalWrite(DIR, HIGH);
-    vstep[3] = 0;
-  } else if (ch == '6') {
-    vstep[3] = -1;
-  } else if (ch == '7') {
-    vstep[3] = -2;
-  } else if (ch == '8') {
-    vstep[3] = -3;
-  } else if (ch == '9') {
-    vstep[3] = -4;
-  }    
+  switch (ch) {
+    case '['
 }
 
 void  stepmov() {    
@@ -120,8 +91,9 @@ void  stepmov() {
     digitalWrite(STEP, HIGH);
     delayMicroseconds(vstep[1]);
     digitalWrite(STEP, LOW);
-    pulseset()
+    pulseset();
   }
+    for(count = 0; count < vstep[0];
   Serial.println(vstep[1]);
   digitalWrite(ENA1, HIGH);
   digitalWrite(ENA2, HIGH);
@@ -129,6 +101,7 @@ void  stepmov() {
 }
 
 void pulseset() {
+    for (i = 0; i < step[0]; i++)
     if (vstep[1] >= vstep[2]) {
         vstep[1] = vstep[1] - vstep[3];
     } else {
