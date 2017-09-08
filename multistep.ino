92a43ed33be745f4413ffc0a79573fafdd133c40
YongJin Jung <30520764+greensori@users.noreply.github.com>
2017-09-08 17:13:16 +0900

    Update multistep.ino

--- multistep.ino
+++ multistep.ino
@@ -5,106 +5,140 @@
 //before stepping operateion ending, must send serial signals
 //step = pulse * 2
 
+
+
 #define ENA1    5
-#define DIR1    6
 #define STEP1   7
 #define ENA2    8
-#define DIR2    9
 #define STEP2   10
 #define ENA3    11
-#define DIR3    12
 #define STEP3   13
 
 #define limitrpm 870
+const int dir[3] = {6, 9, 12};
 
 char ch;
 int count;
-//steps, rpm, max, acc
-int totalswitch;
+int i;
+int stepno;
+int goswitch;
 //setting 3stepping motors (steps, pulse, maxpulse, acc)
-//int vstep[3];
-int vpulse[3];
-int vlimit[3];
-int vacc[3];
-int vdir[3];
-int signal;
-
+int vpulse[3] = {1000, 1000, 1000};
+int vstep[3];
+int vacc[3] = {0, 0, 0};
+int vlimit[3] = {870, 870, 870};
 
 void setup() {
     Serial.begin(115200);
     pinMode(ENA1, OUTPUT);
-    pinMode(STEP1, OUTPUT);
-    pinMode(DIR1, OUTPUT);
     pinMode(ENA2, OUTPUT);
-    pinMode(STEP2, OUTPUT);
-    pinMode(DIR2, OUTPUT);
     pinMode(ENA3, OUTPUT);
+    pinMode(STEP1, OUTPUT);
+    pinMode(STEP2, OUTPUT);
     pinMode(STEP3, OUTPUT);
-    pinMode(DIR3, OUTPUT);
+    for (i= 0; i < 3; i++) {
+       pinMode(dir[i], OUTPUT);
+    }
     //stepper off
     digitalWrite(ENA1, HIGH);
-    digitalWrite(DIR1, HIGH);
     digitalWrite(ENA2, HIGH);
-    digitalWrite(DIR2, HIGH);
     digitalWrite(ENA3, HIGH);
-    digitalWrite(DIR3, HIGH);
-    }
-
+}
 
 void loop() {
   if (Serial.available() > 0) {
     ch = Serial.read();
-    if (48 <= ch && ch <= 57) {
-      stepset();
-    } else {
-      digitalWrite(ENA1, LOW);
-      digitalWrite(ENA2, LOW);
-      digitalWrite(ENA3, LOW);
-      charconverter();
-      stepmov();
+    charconverter();
+    if (goswitch > 0) {
+      stepmove(); 
+    } 
     }
-  // else { analog.read(potentiometer)) <- calculating status
-}}
+}
 
-//to give a total switch values
+
 void charconverter() {
-  switch (ch) {
-      case 'q':
-          step[0] = 50;
-      case 'w':
-          step[0] = 100;
-  }
-           signal = (step[0] / 2)
-}
+    if (35 <= ch && ch <= 37) {
+      switch (ch) {
+        case 35:
+          stepno = 0;
+          Serial.println ("setting stepping 1");
+          break;
+        case 36:
+          stepno = 1;
+          Serial.println ("setting stepping 2");   
+          break;
+        case 37:
+          stepno = 2;
+          Serial.println ("setting stepping 3");          
+          break;
+      }
+    } else if (48<= ch && ch <= 57) {
+      switch (ch) {
+        case 48:
+          vacc[stepno] = 0;
+          break;
+        case 49:
+          vacc[stepno] = 1;
+          break;
+        case 50:
+          vacc[stepno] = 2;
+          break;
+        case 51:
+          vacc[stepno] = 3;
+          break;
+        case 52:
+          vacc[stepno] = -1;
+          break;
+        case 53:
+          vacc[stepno] = -2;
+          break;
+        case 54:
+          vacc[stepno] = -3;
+          break;
+        case 55:
+          vacc[stepno] = (vacc[stepno] * 2);
+          break;
+        case 56:
+          digitalWrite(dir[stepno], HIGH);
+          break;
+        case 57:
+          digitalWrite(dir[stepno], LOW);
+          break;
+      } //end switch
+    } else if (97<= ch && ch <= 122) {
+    switch (ch) {
+      case 97:
+        vpulse[stepno] = 800;
+        break;
+      case 98:
+        vpulse[spepno] = 700;
+        break;
+      default:
+        break;
+            
+     } //end switch
+  } //end if
+} // end proc
 
-//
-void stepset() {
-  switch (ch) {
-    case '['
-}
 
-void  stepmov() {    
-  if (vstep[0] <= 1) {
-    vstep[0] = 2;
+//!33[91]93
+void stepmove() {
+  if (goswitch == 1) {
+    digitalWrite(ENA1, LOW);
+      for (count = 0; count < vstep[0]; count++) {
+        digitalWrite(STEP1, HIGH);
+        delayMicroseconds(vpulse[0]);
+        digitalWrite(STEP1, LOW);
+        pulseset();
+        }
+    Serial.println(vpulse[stepno]);
+    digitalWrite(ENA1, HIGH);
   }
-   for (count = 0; count < vstep[0]; count++) {
-    digitalWrite(STEP, HIGH);
-    delayMicroseconds(vstep[1]);
-    digitalWrite(STEP, LOW);
-    pulseset();
-  }
-    for(count = 0; count < vstep[0];
-  Serial.println(vstep[1]);
-  digitalWrite(ENA1, HIGH);
-  digitalWrite(ENA2, HIGH);
-  digitalWrite(ENA3, HIGH);
-}
+} // end proc
 
 void pulseset() {
-    for (i = 0; i < step[0]; i++)
-    if (vstep[1] >= vstep[2]) {
-        vstep[1] = vstep[1] - vstep[3];
+    if (vpulse[stepno] >= vlimit[stepno]) {
+        vpulse[stepno] = vpulse[stepno] - vacc[stepno];
     } else {
-        vstep[1] = vstep[2];
-    }
+        vpulse[stepno] = vlimit[stepno];
 }
