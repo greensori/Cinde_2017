b4fb1b019724d12320acbe2adb199163b480ade4
YongJin Jung <30520764+greensori@users.noreply.github.com>
2017-09-10 17:18:47 +0900

    simple controller

--- controller.py
+++ controller.py
@@ -11,7 +11,6 @@ def keydown(e):
 root = Tk()
 frame = Frame(root, width=100, height=100)
 frame.bind("<KeyPress>", keydown)
-#frame.bind("<KeyRelease>", keyup)
 frame.pack()
 frame.focus_set()
 root.mainloop()
