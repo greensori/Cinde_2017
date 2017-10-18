dbe88ccc8714331b4be178f83bd169eeeac7deb2
YongJin Jung <30520764+greensori@users.noreply.github.com>
2017-10-18 17:27:00 +0900

    Update worker.ino

--- Middleware/green_version2/worker.ino
+++ Middleware/green_version2/worker.ino
@@ -3,19 +3,30 @@
 green stepper1(11, 12, 13, 1200);
 green stepper2(4, 5, 6, 1100);
 
+#define dirmaker 34
+
 char ch;
 int accel;
+int result_dir;
 int count;
 
 void setup() {
   Serial.begin(115200);
 }
 
+
+//43.44.45 = + , -
+//35.36 = # $
+
 void loop() {
   if (Serial.available()) {
     if (count == 4) {count = stepper1.setting(3);}
     ch = Serial.read();
-    if (ch >=  48 && ch <= 57) {
+    if (ch == 35 || ch == 36) {
+      result_dir = (ch - dirmaker);
+      stepper1.setting(result_dir);
+      stepper1.procWork(accel);
+    } else if (ch >=  48 && ch <= 57) {
       stepper1.procWork(accel); 
       count = stepper1.asciino(ch, count);
     } else {
@@ -23,8 +34,6 @@ void loop() {
     }
   } else {
     count = stepper1.setting(4); //operation end
-    Serial.println (count);
-    delay(1000);
   }
 }
 
