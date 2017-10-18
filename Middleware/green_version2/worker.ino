670aee239324732b5aab82fa42891fa6e0a6d2e3
YongJin Jung <30520764+greensori@users.noreply.github.com>
2017-10-19 00:18:39 +0900

    Update worker.ino

--- Middleware/green_version2/worker.ino
+++ Middleware/green_version2/worker.ino
@@ -1,14 +1,17 @@
 #include "green.h"
 
-green stepper1(11, 12, 13, 1200);
+green stepper1(13, 11, 12, 4000);
 green stepper2(4, 5, 6, 1100);
 
 #define dirmaker 34
+#define periodmaker 64
+#define periodmakerL 96
 
 char ch;
-int accel;
 int result_dir;
 int count;
+
+int accel = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -17,7 +20,8 @@ void setup() {
 
 //43.44.45 = + , -
 //35.36 = # $
-
+// A ~ = 65 ~
+// a ~ = 97 ~
 void loop() {
   if (Serial.available()) {
     if (count == 4) {count = stepper1.setting(3);}
@@ -29,11 +33,15 @@ void loop() {
     } else if (ch >=  48 && ch <= 57) {
       stepper1.procWork(accel); 
       count = stepper1.asciino(ch, count);
-    } else {
-      stepper1.idle(accel, 4);
+    } else if (ch >=43 && ch <= 45) {
+      
+    } else if (ch == 65) {
+      stepper1.idle(accel, 2);
     }
   } else {
-    count = stepper1.setting(4); //operation end
+    if (count != 4) {
+      count = stepper1.setting(4); //operation end
+    }
   }
 }
 
