9d336aab030f36ba50569e0fe6ea3b92f9a90fad
YongJin Jung <30520764+greensori@users.noreply.github.com>
2017-10-25 17:55:53 +0900

    Update green.h

--- Middleware/green_version2/green.h
+++ Middleware/green_version2/green.h
@@ -11,15 +11,23 @@ class green
     int _ENA;
     int _DIR;
     int _STEP;
-    int _status;
-    int _d;
+    int _Servo; //servo motor pi value
+    int _pwmS; //savings servo angle status
+    int _Potentio; // potentio pin no
+    int _potentioSensor[4]; // savings recent potetiometer value
+    int _pcount; //count for recording seprate values on potentio~[4]
+    int _status; //for saving remain pwm
+    int _d; 
+    int _lv; //receiving present status
     int _i;
   public:
-    green(int EnaNo, int DirNo, int StepNo, int pulse);
+    green(int pulse, int EnaNo, int DirNo, int StepNo, int ServoNo, int PotenNo);
     int setting(int setvalue);
     void procWork(int Accel);
     void idle(int Accel, int peri);
     int asciino(char c1, int digit);
+    int preader(); //read information from potentiometer
+    int potentioavg(int poten);
 };
 
 #endif
